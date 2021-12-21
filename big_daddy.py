a1567ba84a2556889d6817f177c9eb57c9aa2183
lspang1999 <89992392+lspang1999@users.noreply.github.com>
2021-12-21 16:20:22 -0500

    Add files via upload
    First draft of working big_daddy script to guess keys. Guesses on one pass of single key testing

--- big_daddy.py
+++ big_daddy.py
@@ -9,6 +9,7 @@ parser.add_argument("locked_circuit")
 parser.add_argument("output_path")
 parser.add_argument("top_level_module")
 parser.add_argument("num_keys")
+parser.add_argument("min_faults")
 args = parser.parse_args()
 
 # TODO: Either edit generate_shorted_key_files with output manually, or add argument
@@ -17,16 +18,26 @@ subprocess.run(['python3','generate_shorted_key_files.py',args.locked_circuit, a
 # for each file, convert to bench
 for i in range(0, int(args.num_keys)):
 	for j in range(0, 2):
-		print()
-		input_file = args.output_path + args.top_level_module + "_" + "KeyGate" + str(i) + '_' + str(j) + ".v"
-		output_file = args.output_path + args.top_level_module + "_" + "KeyGate" + str(i) + str(j) + ".bench"
-		subprocess.run(['python3','ver2bench.py',input_file, '-o ' + output_file])
+		input_file = args.output_path + args.top_level_module + "_" + "keyGate" + str(i) + '_' + str(j) + ".v"
+		# print(input_file)
+		output_file = args.output_path + args.top_level_module + "_" + "keyGate" + str(i) + str(j) + ".bench"
+		subprocess.run(['python3','ver2bench.py',input_file, '-o', output_file])
 
 # for each bench file, remove floating input
-
+for i in range(0, int(args.num_keys)):
+	for j in range(0, 2):
+		input_file = args.output_path + args.top_level_module + "_" + "keyGate" + str(i) + str(j) + ".bench"
+		output_file = args.output_path + args.top_level_module + "_" + "new_keyGate" + str(i) + str(j) + ".bench"
+		subprocess.run(['python3', 'convert_for_atalanta.py', input_file, output_file])
+		
 # for each bench file, run atalanta
-
+for i in range(0, int(args.num_keys)):
+	for j in range(0, 2):
+		bench_file = args.output_path + args.top_level_module + "_" + "new_keyGate" + str(i) + str(j) + ".bench"
+		subprocess.run(['atalanta', '-v', bench_file])
 # run key guessing
-# subprocess.call(['python3', 'redundancy_key_guessing.py', 'min_faults', args.num_keys, 'first_key_file'])
 
-p
+first_key_file = args.top_level_module + "_" + "new_keyGate00.ufaults"
+subprocess.call(['python3', 'redundancy_key_guessing.py', args.min_faults, args.num_keys, first_key_file])
+
+
--- convert_for_atalanta.py
+++ convert_for_atalanta.py
@@ -9,16 +9,12 @@ output_file = open(args.output_file_name, "w");
 # grab first line to determine floating input to remove
 # ASSUMPTION: key gate number is 2nd to last character of module
 module = input_file.readline()
-print(module)
-# floating_input = "keyInput" + module[len(module) - 3]
-floating_input = "a"
-print(floating_input)
+# print(module)
+floating_input = "keyInput" + module[len(module) - 3]
+# print(floating_input)
 output_file.write(module)
 for line in input_file:
 	if floating_input not in line:
-		output_file.write(line)
-	# remove for circuits that need input removed
-	else:
 		output_file.write(line)
 input_file.close()
 output_file.close()
