6b1380f385860dde4ffedb80e88b4b2e223e5311
lspang1999 <89992392+lspang1999@users.noreply.github.com>
2021-12-20 20:58:18 -0500

    Add files via upload
    Files added from Virtual Machine

---
+++ redundancy_key_guessing.py
@@ -0,0 +1,47 @@
+import argparse
+
+parser = argparse.ArgumentParser("Redundancy Attack Key Guessing")
+parser.add_argument("min_redundant_faults")
+parser.add_argument("num_keys")
+parser.add_argument("first_key_file")
+args = parser.parse_args()
+
+# ASSUMPTION: test files end in two digits, where the first digit is the keyGate number and the second digit is the value at test
+
+key_file_start = args.first_key_file[0:len(args.first_key_file)-10]
+print(key_file_start)
+
+for i in range(0, int(args.num_keys)):
+	faults_0 = 0
+	faults_1 = 0
+	try:
+		fault_0_file = open(key_file_start + str(i) + str(0) + ".ufaults", "r");
+	except:
+		faults_0 = 0
+	else:
+		for line in fault_0_file:
+			faults_0 += 1
+		fault_0_file.close()
+	try:
+		fault_1_file = open(key_file_start + str(i) + str(1) + ".ufaults", "r");
+	except:
+		faults_1 = 0
+	else:
+		for line in fault_1_file:
+			faults_1 += 1
+		fault_1_file.close()
+
+	print("keyGate" + str(i) + " faults_0 = " + str(faults_0))
+	print("keyGate" + str(i) + " faults_1 = " + str(faults_1))
+	# Compare number of redundant faults
+	if faults_0 > int(args.min_redundant_faults) and faults_0 > faults_1:
+		print("keyGate" + str(i) + " = 1")
+	if faults_1 > int(args.min_redundant_faults) and faults_1 > faults_0:
+		print("keyGate" + str(i) + " = 0")
+
+
+# TODO: Add solved keys into unsolved keys bench files
+# TODO: Loop solving until pass does not result in more solved keys
+	
+		
+	
